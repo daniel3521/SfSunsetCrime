4b5d56dc481fc735f4b52d82231d479e38eb9c3f
daniel3521 <66038597+daniel3521@users.noreply.github.com>
2020-09-01 16:31:39 -0700

    Add files via upload

---
+++ crime.sql
@@ -0,0 +1,49 @@
+USE SFcrime;
+
+
+# Selecting a certain year 
+-- SELECT COUNT(*) AS '2018' FROM crimetable
+-- WHERE PdDistrict='TARAVAL' 
+-- AND 
+-- DATE LIKE '%/2018';
+
+# Finding the count per year 
+-- SELECT SUBSTRING(Date,7,10) AS 'Year',COUNT(*) AS 'Total' FROM crimetable
+-- WHERE PdDistrict='TARAVAL' 
+-- GROUP BY SUBSTRING(Date,7,10)
+-- ORDER BY SUBSTRING(Date,7,10);
+
+# Looking through certain years 
+-- SELECT * FROM crimetable
+-- WHERE PdDistrict='TARAVAL' 
+-- AND 
+-- DATE LIKE '%/2017'
+-- ORDER BY Date DESC;
+
+#Showing total crime per district 
+-- SELECT PdDistrict AS 'Police District',COUNT(*) AS 'Total' FROM crimetable
+-- GROUP BY PdDistrict
+-- ORDER BY COUNT(*);
+
+#Selecting only the violent crimes per district 
+-- SELECT PdDistrict AS 'Police District',COUNT(*) AS 'Total' FROM crimetable
+-- WHERE NOT Category='NON-CRIMINAL'
+-- GROUP BY PdDistrict
+-- ORDER BY COUNT(*);
+
+#Looking thru the Tenderloin data
+-- SELECT * FROM crimetable
+-- WHERE PdDistrict='TENDERLOIN' AND Category='NON-CRIMINAL'
+-- ORDER BY SUBSTRING(Date,7,10) DESC;
+
+#Looking for the null PdDistrict
+-- SELECT * FROM crimetable
+-- WHERE PdDistrict=''
+-- ORDER BY SUBSTRING(Date,7,10) DESC;
+
+#Update Null entry after looking at the Address
+-- UPDATE crimetable
+-- SET PdDistrict='BAYVIEW'
+-- WHERE PdDistrict='';
+
+
